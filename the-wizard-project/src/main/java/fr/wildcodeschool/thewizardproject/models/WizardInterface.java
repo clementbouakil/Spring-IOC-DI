7c95242a41b038b307fadaf0ffadf7ea50d9d65c
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-07-15 11:20:15 +0200

    add interface + void method giveAdvice + changeDress

---
+++ the-wizard-project/src/main/java/fr/wildcodeschool/thewizardproject/models/WizardInterface.java
@@ -0,0 +1,13 @@
+package fr.wildcodeschool.thewizardproject.models;
+
+public class WizardInterface {
+
+    public void giveAdvice() {
+        
+    }
+
+    public void changeDress() {
+        
+    }
+
+}
