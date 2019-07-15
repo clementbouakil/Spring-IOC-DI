16d62ba7afb08896d7b6dc1835d96c228352178b
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-07-15 11:26:14 +0200

    create class Dumbledore + Gandalf + implemente methods

---
+++ the-wizard-project/src/main/java/fr/wildcodeschool/thewizardproject/models/Dumbledore.java
@@ -0,0 +1,15 @@
+package fr.wildcodeschool.thewizardproject.models;
+
+public class Dumbledore implements WizardInterface {
+
+    @Override
+    public String giveAdvice() {
+        return null;
+    }
+
+    @Override
+    public String changeDress() {
+        return null;
+    }
+    
+}
---
+++ the-wizard-project/src/main/java/fr/wildcodeschool/thewizardproject/models/Gandalf.java
@@ -0,0 +1,15 @@
+package fr.wildcodeschool.thewizardproject.models;
+
+public class Gandalf implements WizardInterface {
+
+    @Override
+    public String giveAdvice() {
+        return null;
+    }
+
+    @Override
+    public String changeDress() {
+        return null;
+    }
+    
+}
